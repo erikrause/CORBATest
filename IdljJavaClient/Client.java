f20812e85913b89b06d7500ca9e6f3ebfbf9f12d
Erkin Kamilov <erkimkamilov@gmail.com>
2022-10-07 18:10:11 +0500

    Java stubs generated, Java client implemented.

---
+++ IdljJavaClient/Client.java
@@ -0,0 +1,31 @@
+import org.omg.CORBA.*;
+import CORBATest.*;
+
+public class Client
+{
+  static Echo echoImpl;
+  static String message = "Hello from Java";
+
+  public static void main(String args[])
+    {
+      try
+      {
+        // create and initialize the ORB
+        ORB orb = ORB.init(args, null);
+
+        org.omg.CORBA.Object objRef = orb.string_to_object(args[0]);
+        echoImpl = EchoHelper.narrow(objRef);
+
+        System.out.println("Sending: "+message);
+        String result = echoImpl.echoString(message);
+        System.out.println("Got back: "+result);
+
+        } 
+        catch (Exception e) {
+          System.out.println("Error: " + e) ;
+          e.printStackTrace(System.out);
+        }
+    }
+
+}
+ 
